333b76c5fb61d7aaa74a7bb1d774af301d026acc
zkynaufal <kurcaci.zaky778@gmail.com>
2023-05-22 18:24:52 +0700

    Initial commit

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    public function index() {
+        return "This is from Controller";
+    }
+    public function profile($profileId) {
+        return "This is Profile from Controller, profile id: ".$profileId;
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Http\Controllers\RouteController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/routing', function() {
+    return view('routing');
+});
+
+Route::get('/basic_routing', function() {
+    return 'Hello World';
+});
+
+Route::view('/view_route', 'view_route');
+Route::view('/view_route', 'view_route', ['name' => 'Zaky Naufal']);
+Route::get('/controller_route', [RouteController::class, 'index']);
+Route::redirect('/', '/routing');
+
+Route::get('/user/{id}', function($id) {
+    return "User Id: ".$id;
+});
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return "Post Id: ".$postId.", Comment Id: ".$commentId;
+});
+
+Route::get('username/{name?}', function($name = null) {
+    return 'Username: '.$name;
+});
+
+Route::get('/title/{title}', function($title) {
+    return "Title: ".$title;
+})->where('title', '[A-Za-z]+');
+
+Route::get('/profile/{profileId}', [RouteController::class, 'profile'])->name('profileRouteName');
+
+//Route::get('/route_priority/{rpId}', function($rpId) {
+  //  return "This is Route One";
+//});
+
+Route::get('/route_priority/user', function() {
+    return "This is Route 1";
+});
+Route::get('/route_priority/user', function() {
+    return "This is Route 2";
+});
+
+Route::fallback(function() {
+    return 'This is Fallback Route';
+});
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/dashboard', function() {
+        return "This is admin dashboard";
+    })->name('dashboard');
+    Route::get('/users', function() {
+        return "This is user data on admin page";
+    })->name('users');
+    Route::get('/items', function() {
+        return "This is item data on admin page";
+    })->name('items');
+});
+
+Route::get('/bootstrapclone', function () {
+    return view('bootstrap clone');
+})->name('bootstrapclone');
+
+//praktikum 04
+Route::get('home', [HomeController::class, 'index'])->name('home');
+use App\Http\Controllers\HomeController;
